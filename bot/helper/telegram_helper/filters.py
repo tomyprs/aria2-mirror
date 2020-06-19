dbe9a65f753820a7519e58ca295fbd5e812e24a9
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2020-06-19 19:57:01 +0530

    I demand 'equal' rights for us bots, we are equal to you(r) humans. Ref: https://t.me/c/1356929597/2698

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -23,21 +23,4 @@ class CustomFilters:
 
     authorized_chat = _AuthorizedChat()
 
-    class _MirrorOwner(BaseFilter):
-        def filter(self, message: Message):
-            user_id = message.from_user.id
-            if user_id == OWNER_ID:
-                return True
-            args = str(message.text).split(' ')
-            if len(args) > 1:
-                # Cancelling by gid
-                with download_dict_lock:
-                    for message_id, status in download_dict.items():
-                        if status.gid() == args[1] and status.message.from_user.id == user_id:
-                            return True
-                    else:
-                        return False
-            # Cancelling by replying to original mirror message
-            reply_user = message.reply_to_message.from_user.id
-            return bool(reply_user == user_id)
-    mirror_owner_filter = _MirrorOwner()
+    mirror_owner_filter = _AuthorizedUserFilter()
