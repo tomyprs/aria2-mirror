707918d9f7f1f9b923d3321739adf1321a678649
Shrimadhav U K <SpEcHIDe@users.noreply.github.com>
2020-08-08 14:44:02 +0530

    fix one more attribute error

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -22,7 +22,7 @@ def mirror_status(client: Client, update: Message):
         reply_message = sendMessage(message, client, update)
         threading.Thread(target=auto_delete_message, args=(client, update, reply_message)).start()
         return
-    index = message.chat.id
+    index = update.chat.id
     with status_reply_dict_lock:
         if index in status_reply_dict.keys():
             deleteMessage(status_reply_dict[index])
