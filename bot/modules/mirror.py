3aec8fd03fb4418f70c9c78eafa68209d6d38965
Shrimadhav U K <SpEcHIDe@users.noreply.github.com>
2020-08-08 14:46:42 +0530

    fixes: AttributeError

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -118,7 +118,7 @@ class MirrorListener(listeners.MirrorListeners):
     def onDownloadError(self, error):
         error = error.replace('<', ' ')
         error = error.replace('>', ' ')
-        LOGGER.info(self.update.effective_chat.id)
+        LOGGER.info(self.update.chat.id)
         with download_dict_lock:
             try:
                 download = download_dict[self.uid]
