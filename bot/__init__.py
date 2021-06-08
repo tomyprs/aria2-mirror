8ec54a19220e6f2ede46b02190d8440600da7a24
TomyPrs <tomy.prs25@gmail.com>
2021-06-08 20:51:59 +0700

    loglevel WARNING
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- bot/__init__.py
+++ bot/__init__.py
@@ -29,7 +29,7 @@ if os.path.exists("log.txt"):
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
+    level=logging.WARNING,
 )
 
 LOGGER = logging.getLogger(__name__)
